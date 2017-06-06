a428fef784e7ccfc9631fb0404e151be674f48cd
valentinitnelav <stefan.valentin87@yahoo.com>
2017-06-06 11:04:56 +0200

    rename columns, adjust correct temp and add some comments

--- Scripts/Whittaker_biomes_prepare.R
+++ Scripts/Whittaker_biomes_prepare.R
@@ -1,6 +1,14 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Prepare Whittaker biomes polygons for plotting in ggplot.
+# Prepare digitized Whittaker biomes polygons for plotting in ggplot.
 # Transform from shapefile to dataframe.
+
+# The Whittaker biomes polygons were digitized in QGIS after georeferencing
+# the graph in Fig 5.5 p. 92 from Ricklefs – The Economy of Nature 
+# (at: https://www.academia.edu/15092278/Ricklefs_The_Economy_of_Nature_6th_txtbk)
+# Georeferencing was done using the EPSG:53002 (sphere equidistant cylindrical) CRS.
+# I used 4 ground control points located in 4 corners of the graph, with X within -100 and 300 
+# instead of -10 and 30 in order to avoid distortions.
+# Digitization was carried at scale 1:500. The file was saved as biomes.shp
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 library(sp)
@@ -13,6 +21,14 @@ biomes.poly <- rgdal::readOGR(dsn   = "Whittaker biomes graph - digitize",
 
 # transform to data frame
 df.biomes_polyg <- broom::tidy(biomes.poly)
+
+# divide long (temperature) by 10 to obtain real values.
+# the original values multiplied by 10 had to be used for avoiding distortions in QGIS
+df.biomes_polyg$long <- df.biomes_polyg$long/10
+
+# rename columns
+names(df.biomes_polyg)[1] <- "temp_C"
+names(df.biomes_polyg)[2] <- "precp_cm"
 
 # save to csv file
 write.csv(x = df.biomes_polyg, file = "Data/Whittaker_biomes.csv", row.names = FALSE)

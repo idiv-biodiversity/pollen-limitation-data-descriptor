8290dcfbfaffe80e4d61b5ed81178eaa17525043
valentinitnelav <stefan.valentin87@yahoo.com>
2017-10-11 15:46:29 +0200

    move grid lines begind polygons

--- Scripts/Global_map_pollen_lim.R
+++ Scripts/Global_map_pollen_lim.R
@@ -1,21 +1,20 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Make global map for pollen limitation dataset
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+####################################################################################
+## Global map for pollen limitation dataset
+####################################################################################
 
 # List of packages for session (add here any new desired packages)
-.packages = c("rgdal", "sp", "data.table", "ggplot2", "ggmap", "ggalt")
+.myPackages = c("rgdal", "sp", "data.table", "ggplot2", "ggmap", "ggalt")
 # Install CRAN packages (if not already installed)
-.inst <- .packages %in% installed.packages()
-if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
+.inst <- .myPackages %in% installed.packages()
+if(length(.myPackages[!.inst]) > 0) install.packages(.myPackages[!.inst])
 # Load packages into session 
-sapply(.packages, require, character.only=TRUE)
+sapply(.myPackages, require, character.only=TRUE)
 
-
-# ==================================
+# =================================================================================
 # Read & prepare data
-# ==================================
-# read latest dataset from Joanne
-PL.all.dt <- fread("Data/PL_ANALYSIS_24_04_2017.csv", colClasses = "character")
+# =================================================================================
+# read latest dataset (prepared by Joanne)
+PL.all.dt <- fread("Data/PL_ANALYSIS_02_10_2017.csv", colClasses = "character")
 
 # get only columns of interest
 PL.dt <- PL.all.dt[,.(unique_number, lon_decimal_PTL_JMB, lat_decimal_PTL_JMB)]
@@ -60,22 +59,6 @@ names(lbl.X)[5:6] <- c("X.prj","Y.prj")
 # plot
 # ==================================
 ggplot() +
-    # ___ add Natural Earth countries projected to Robinson
-    geom_polygon(data      = NE_countries_rob, 
-                 aes(x     = long,
-                     y     = lat, 
-                     group = group), 
-                 colour    = "grey60", 
-                 fill      = "gray95", 
-                 size      = 0.25) +
-    # ___ add Natural Earth box projected to Robinson
-    geom_polygon(data   = NE_box_rob, 
-                 aes(x  = long, 
-                     y  = lat), 
-                 colour ="black", 
-                 fill   ="transparent", 
-                 size   = 0.25) +
-    # "Regions defined for each Polygons" warning has to do with fortify transformation. Might get deprecated in future!
     # ___ add graticules projected to Robinson
     geom_path(data      = NE_graticules_rob, 
               aes(x     = long, 
@@ -84,6 +67,14 @@ ggplot() +
               linetype  = "dotted", 
               color     = "grey50", 
               size      = 0.25) +
+    # ___ add Natural Earth countries projected to Robinson
+    geom_polygon(data      = NE_countries_rob, 
+                 aes(x     = long,
+                     y     = lat, 
+                     group = group), 
+                 colour    = "grey60", 
+                 fill      = "gray95", 
+                 size      = 0.25) +
     # ___ add graticule labels - latitude and longitude
     geom_text(data      = lbl.Y, 
               aes(x     = X.prj, 
@@ -97,14 +88,22 @@ ggplot() +
                   label = lbl), 
               color     = "grey50", 
               size      = 2.5) +
+    # ___ add Natural Earth box projected to Robinson
+    geom_polygon(data   = NE_box_rob, 
+                 aes(x  = long, 
+                     y  = lat), 
+                 colour ="black", 
+                 fill   ="transparent", 
+                 size   = 0.25) +
+    # "Regions defined for each Polygons" warning has to do with fortify transformation. Might get deprecated in future!
     # ___ add the XY points
     geom_point(data   = PL.dt.unq, 
                aes(x  = X.prj, 
                    y  = Y.prj), 
                size   = 2,
                shape  = 21,   # the shape of the point is a circle
-               colour = "Blue 4", 
-               bg     = "Deep Sky Blue 4", # give color for the cricle edge and also for bakground (bg)
+               colour = "black", 
+               bg     = "gray40", # give color for the cricle edge and also for bakground (bg)
                alpha  = 1) + # set opacity level 
     # ___ the default ratio = 1 in coord_fixed ensures that one unit on the x-axis is the same length as one unit on the y-axis
     coord_fixed(ratio = 1) +
@@ -115,5 +114,5 @@ ggplot() +
     theme(plot.margin = unit(c(t=0, r=0, b=0, l=0), unit="cm"))
 
 # save to pdf and png file
-ggsave("Output/Global_map_draft_1.pdf", width=29.7, height=14, units="cm", scale=1.2)
-ggsave("Output/Global_map_draft_1.png", width=29.7, height=14, units="cm", scale=1.2, dpi=600)
+ggsave("Output/Global_map_draft_11Oct17.pdf", width=14, height=7, units="cm", scale=2.5)
+ggsave("Output/Global_map_draft_11Oct17.png", width=14, height=7, units="cm", scale=2.5, dpi=600)

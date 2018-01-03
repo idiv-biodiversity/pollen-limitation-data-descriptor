e466054431b6472935961966edac6eaf39357320
valentinitnelav <stefan.valentin87@yahoo.com>
2018-01-03 10:41:51 +0100

    major annotation update - merge data to tree and use geom segment & text - fix bug of root branches coloring - color major clade groups manually

--- Scripts/Phylogeny_graph.R
+++ Scripts/Phylogeny_graph.R
@@ -1,30 +1,35 @@
-####################################################################################
-## Phylogeny graph for pollen limitation dataset
-####################################################################################
+###############################################################################
+## Phylogeny tree graph for pollen limitation dataset
+###############################################################################
 
 library(readxl)
+library(data.table)
+library(ggplot2)
+library(glue)
+library(lazyeval)
+library(RColorBrewer)
+
 library(picante)
 library(ape)
 library(pez)
 library(taxize)
-library(data.table)
-library(lazyeval)
-library(ggplot2)
-library(glue)
-# Intsall ggtree
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("ggtree")
-# attach ggtree
-library(ggtree)
-# more about ggtree at: https://bioconductor.org/packages/release/bioc/html/ggtree.html
-
 library(Taxonstand)
 library(wikitaxa)
 
-# =================================================================================
+# Intsall ggtree from bioconductor
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("ggtree")
+# Installing from GitHub does not work at this time (Dec. 2017).
+
+# load/attach ggtree package
+library(ggtree)
+# more about ggtree at: 
+# https://bioconductor.org/packages/release/bioc/html/ggtree.html
+
+# =============================================================================
 # Read & prepare data
-# =================================================================================
-# Read species names and their corresponding family
+# =============================================================================
+# Read species names and their corresponding family (file from Tiffany)
 data_aggregated <- data.table(readxl::read_excel(path = "Data/phylogeny/specieslist.xlsx", 
                                                  sheet     = 1, 
                                                  col_names = TRUE))
@@ -32,22 +37,24 @@ data_aggregated <- data.table(readxl::read_excel(path = "Data/phylogeny/speciesl
 dataspecies <- unique(data_aggregated, by = "Species_accepted_names")
 
 # Read phylogeny information to be used for labeling the plot
+# Phylogeny information_VS.xlsx contains some corrections as oposed to Phylogeny information.xlsx
 Phylo_info <- data.table(readxl::read_excel(path = "Data/phylogeny/Phylogeny information_VS.xlsx", 
                                             sheet     = 1, 
                                             col_names = TRUE))
-# If needed, replace spaces with _ in column names
-data.table::setnames(Phylo_info, gsub(" ", "_", names(Phylo_info)))
+# Defensively replace spaces with "_" in column names
+data.table::setnames(Phylo_info, gsub("\\s+", "_", names(Phylo_info)))
 
 # Check if any differences by species names between the 2 files
 # This is important because the key to merge the files is the species names
-all.equal(sort(Phylo_info$Species_accepted_names), 
-          sort(dataspecies$Species_accepted_names))
+all.equal(sort(Phylo_info$Species_accepted_names, na.last = TRUE), 
+          sort(dataspecies$Species_accepted_names, na.last = TRUE))
 # if not TRUE then check differences
 setdiff(dataspecies$Species_accepted_names, Phylo_info$Species_accepted_names)
 setdiff(Phylo_info$Species_accepted_names, dataspecies$Species_accepted_names)
-# "Physocarpus_amurensis" "Silene_stockenii" where added in Phylogeny information_VS
+# "Physocarpus_amurensis" "Silene_stockenii" where added in Phylogeny information_VS.xlsx
 # as they were present in the tree object already.
-# Need checking if I added correct info!
+# Need checking Phylogeny information_VS.xlsx if I added correct info for these species!
+# for now, these two are missing family info
 
 # Merge the two files
 Phylo_info <- merge(x = Phylo_info,
@@ -55,9 +62,9 @@ Phylo_info <- merge(x = Phylo_info,
                     by = "Species_accepted_names",
                     all.x = TRUE)
 
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Check some taxa names
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 my_sp <- fread("data/phylogeny/taxa_to_check.csv")
 my_sp[, taxa4tpl := gsub(pattern = "_", replacement = " ", x = taxa, fixed = TRUE)]
 
@@ -95,29 +102,30 @@ my_sp <- merge(x = my_sp,
 
 fwrite(my_sp, file = "Output/sp_to_check4tree.csv")
 
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Build phylogeny
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Read tree from Tiffany
 tree <- read.tree("Data/phylogeny/Aggre.tree.tre")
+tree$tip.label[1:5] # check formating of first 5 species names
 # tree <- read.tree("Data/phylogeny/phylo1265species.tre")
 SiteTree <- pez::congeneric.merge(tree    = tree, 
                                   species = Phylo_info$Species_accepted_names, 
-                                  split   = " ")
+                                  split   = "_") # space or "_" doesn't seem to make a difference
 all.equal(tree, SiteTree)
 identical(tree, SiteTree)
-# Why the need of congeneric.merge if the two trees are identical?
+# Why need congeneric.merge if the two trees are identical?
 
-# "Physocarpus_amurensis" "Silene_stockenii"  are extra in the tree 
-# when using Phylogeny information.xlsx
+# Note that "Physocarpus_amurensis" "Silene_stockenii" were extra in the tree 
+# when using Phylogeny information.xlsx instead of Phylogeny information_VS.xlsx
 setdiff(SiteTree$tip.label, Phylo_info$Species_accepted_names)
 setdiff(Phylo_info$Species_accepted_names, SiteTree$tip.label)
 
-# Merge tree tip labels with data from Phylogeny information.xlsx 
+# Merge tree tip labels with data from Phylo_info;
 # tip.label needs to be used exactly as such (with this name).
 # This is needed for merging data with the tree based on tip labels with  %<+% operator
 tip_lbs <- data.table(tip.label = SiteTree$tip.label)
-# Phylo_info_merged will contain extra 2 rows if Phylogeny information.xlsx was used
+# Note that Phylo_info_merged may contain extra 2 rows if Phylogeny information.xlsx was used
 # (the 2 extra species discussed above)
 Phylo_info_merged <- merge(x = tip_lbs, 
                            y = Phylo_info, 
@@ -129,165 +137,122 @@ Phylo_info_merged <- merge(x = tip_lbs,
 # Phylo_info_merged[is.na(order_group), order_group := "Other"]
 
 # Split by APG4_group data - for coloring purposes
-# inspired from https://bioconductor.org/packages/devel/bioc/vignettes/ggtree/inst/doc/treeAnnotation.html
+# see https://bioconductor.org/packages/devel/bioc/vignettes/ggtree/inst/doc/treeAnnotation.html
 # or http://www.ggplot2-exts.org/ggtree.html
+length(Phylo_info_merged$tip.label)
+length(Phylo_info_merged$APG4_group)
 APG4_gr <- split(Phylo_info_merged$tip.label, Phylo_info_merged$APG4_group)
 names(APG4_gr)
 length(APG4_gr)
 SiteTree_gr <- ggtree::groupOTU(SiteTree, APG4_gr)
-
-# =================================================================================
-# Plot tree
-# =================================================================================
 str(SiteTree_gr)
-tree_pl <- ggtree(tr      = SiteTree_gr, 
-                  mapping = aes(color = group), 
-                  layout  = 'circular') +
-    geom_tiplab(aes(angle=angle), # to plot labels properly alligned from the center of the circle
-                size = 1) +
-    theme(legend.position = "right")
-# attaches annotation data to a tree view
-# so that all the columns in the Phylo_info_merged are visible to ggtree
-tree_pl2 <- tree_pl %<+% Phylo_info_merged +
-    geom_tippoint(aes(color = order_group),
-                  position = position_nudge(x = 1),
-                  shape = 16,
-                  size = 0.5,
-                  show.legend = FALSE) +
-    geom_tippoint(aes(color = order_group),
-                  position = position_nudge(x = 40),
-                  shape = 16,
-                  size = 0.5,
-                  show.legend = FALSE) +
-    geom_text2(aes(angle = angle, 
-                   subset = !isTip, 
-                   label = node), 
-               hjust = 1, 
-               vjust = 0.5, 
-               size  = 0.6,
-               color = "black") +
-    geom_text(aes(angle = angle,
-                  color = order_group,
-                  label = ifelse(is.na(order_group),
-                                 yes = "",
-                                 no  = paste("node:", node,
-                                             "y:", y,
-                                             "order:", order_group,
-                                             "family:", Family))), 
-              hjust = 1, 
-              vjust = 0.5, 
-              size  = 0.6,
-              nudge_x = 75,
-              show.legend = FALSE)
+# There is a "0" group label - not sure why this happens, but below is the fix:
+levels(attributes(SiteTree_gr)$group) # check all group labels
+# overwrite "0" with "Basal" so that the basal segments get the same color.
+# similar to: https://en.wikipedia.org/wiki/Phylogenetic_tree#/media/File:CollapsedtreeLabels-simplified.svg
+levels(attributes(SiteTree_gr)$group)[1] <- "Basal"
+# reorder factor levels if needed
+attributes(SiteTree_gr)$group <- factor(x = attributes(SiteTree_gr)$group, 
+                                        levels = c("Basal", 
+                                                   "Eudicot", 
+                                                   "Magnoliids", 
+                                                   "Monocot"))
 
-ggsave(plot = tree_pl2,
-       filename = "Output/Phylo_tree_draft5.pdf", 
-       width = 10, height = 10, scale = 5, units = "cm")
-# use geom_tippoint and geom_text instead of geom_strip
-# https://bioconductor.org/packages/devel/bioc/vignettes/ggtree/inst/doc/treeAnnotation.html
+# =============================================================================
+# Plot tree
+# =============================================================================
 
-###################
+# Experiment with color
+my_cols <- brewer.pal(n = 4, name = "Set1")
+names(my_cols) <- levels(attributes(SiteTree_gr)$group)
+scales::show_col(my_cols); my_cols
+
+tree_pl <- 
+    ggtree(tr      = SiteTree_gr, 
+           mapping = aes(color = group), 
+           layout  = 'circular') +
+    # adjust coloring of main groups
+    scale_color_manual(name = 'Clade',
+                       values = my_cols)
+
+# Attaches annotation data to a tree view
+# so that all the variables in the Phylo_info_merged are visible to ggtree
+tree_pl <- tree_pl %<+% Phylo_info_merged 
+
 tree_dt <- data.table(tree_pl$data)
+# select only the tips and order by coord y
+tree_dt <- tree_dt[isTip == TRUE][order(y)]
 
-test_tree <- tree_pl2 + 
-    geom_segment(aes(x = max(tree_dt$x) + 80, 
-                     y = 2, 
-                     xend = max(tree_dt$x) + 80, 
-                     yend = 19),
-                 colour = "black",
-                 lineend = "round",
+# Make table with y cords for each group; this helps for drawing & labeling segments.
+# Note the usage of "rleid" function, which is a grouping ID generator,
+# needed because consecutive rows of an identical reoccurring group must form a unique group.
+coord_groups <- tree_dt[, .(y1 = y[1],
+                            y2 = y[.N],
+                            angle = mean(angle),
+                            n = .N), # optional - helps with counting
+                        by = .(order_group, 
+                               id_gr = rleid(order_group, 
+                                             prefix = "grp"))]
+# Compute the middle y – will be used for placing the group label;
+# similarly the mean angle was computed above already. 
+coord_groups[, y_mid := rowMeans(.SD), .SDcols = c("y1", "y2")]
+# For groups that y1=y2, adjust their y coordinates so that a segment gets drawn;
+# if not, no segment get drawn for such cases.
+coord_groups[, y1_adj := ifelse(y1 == y2, y1 - 0.25, y1)]
+coord_groups[, y2_adj := ifelse(y1 == y2, y2 + 0.25, y2)]
+
+# labels need angle adjustment for cases between 90 and 270 dg
+coord_groups[, angle_adj := angle]
+coord_groups[angle %between% c(90, 180), angle_adj := angle_adj + 180]
+coord_groups[angle %between% c(180, 270), angle_adj := angle_adj - 180]
+
+# labels with angles between 90 and 270 dg
+# need change of horizontal adjustment argument from 0 to 1
+coord_groups[, hjust_adj := ifelse(angle %between% c(90, 270), yes = 1, no = 0)]
+
+# if needed, coloring could be binary 
+# coord_groups[, col := ifelse(.I%%2, 0.5, 1)]
+
+my_x <- max(tree_dt$x) + 3
+
+test_tree <- 
+    tree_pl + 
+    # add line segments for each group
+    geom_segment(data = coord_groups,
+                 aes(x = my_x, 
+                     y = y1_adj, 
+                     xend = my_x, 
+                     yend = y2_adj),
+                 color = "black",
+                 lineend = "butt",
+                 size = 3,
                  show.legend = FALSE) +
-    geom_cladelabel(node=10, label="test label")
-    
-ggsave(plot = test_tree,
-       filename = "Output/Phylo_tree_draft6.pdf", 
-       width = 10, height = 10, scale = 5, units = "cm")
-
-
-###############################################
-# Trial with  ggtree::geom_strip
-###############################################
-# str(tree_pl, max.level = 1)
-tree_dt <- data.table(tree_pl$data)
-
-# Split tip indices by desired order groups (NA-s are not considered if any)
-# Orders.tip.idx <- split(1:dim(Phylo_info_merged)[1], Phylo_info_merged$order_group)
-all_taxa_y <- split(tree_dt[isTip == TRUE, y], tree_dt[isTip == TRUE, order_group])
-
-my_taxa <- "Lamiales"
-apply_geom_strip <- function(my_taxa, ...){
-    nodes <- tree_dt[isTip == TRUE, node, by = "order_group"][order_group == my_taxa, node]
-    # split vector of indices in continous sequences
-    # https://stackoverflow.com/questions/25793981/split-a-vector-by-its-sequences
-    nodes_gr <- split(nodes, cumsum(c(TRUE, diff(nodes)!=1)))
-    # create empty list
-    my_strips <- vector(mode = "list", length = length(nodes_gr))
-    # for each sequence create a geom_strip to be plot on the tree
-    for (i in 1:length(nodes_gr)){
-        my_strips[[i]] <- ggtree::geom_strip(taxa1 = nodes_gr[[i]][1], 
-                                             taxa2 = nodes_gr[[i]][length(nodes_gr[[i]])],
-                                             label = my_taxa)
-    }
-    return(my_strips)
-}
-
-
-# y <- tree_dt[isTip == TRUE, y, by = "order_group"][order_group == "Lamiales", sort(y)]
-# y.gr <- split(y, cumsum(c(TRUE, diff(y)!=1)))
-# y.gr
-# tree_pl + geom_line(mapping = aes(x = 243.2697,
-#                                   y = y.gr[[2]]),
-#                     data = tree_dt)
-
-tree_pl2 <- tree_pl + 
-    geom_text(aes(angle=angle,
-                  label=ifelse(is.na(order_group),
-                               yes = "",
-                               no  = paste("n:",node,"y:",y,"gr:",order_group))), 
-              hjust=1, vjust=0.5, size=0.6, color = "red") +
-    # geom_text(aes(label=y), hjust=1, vjust=1.4, size=0.6, color = "red") +
-    # this fails for wather resons...
-    # geom_strip(taxa1 = 'Gesneria_pedunculosa', taxa2 = 'Gesneria_vernicosa', label = "Lamiales", offset = 0.5, align=TRUE, barsize = 1)
-    # does not work with the y coordinates
-    # geom_strip(taxa1 = 1260, taxa2 = 1267, label = "Lamiales", offset = 0.5, align=TRUE, barsize = 1) +
-    # but it works with node index
-    # geom_strip(taxa1 = 1236, taxa2 = 1243, label = "Lamiales", offset = 0.5, align=TRUE, barsize = 1) +
-    # however, because how node and y are related, if the node interval stops earlier (e.g. reaches 1267 for Lamiales case)
-    # than its y contor then the strip stops there as weel - try this:
-    # geom_strip(taxa1 = 1144, taxa2 = 1267, label = "Lamiales", offset = 0.5, align=TRUE, barsize = 1)
-    apply_geom_strip( my_taxa="Other", offset=0.5, offset.text=10, align=TRUE, barsize=1, hjust=0.5, color='red') +
-    lapply(X = tree_dt[isTip == TRUE & order_group != "Other", unique(order_group)],
-           FUN = apply_geom_strip, offset=0.5, offset.text=10, align=TRUE, barsize=1, hjust=0.5)
-# geom_strip(taxa1 = 41, taxa2 = 42, label = "Other", offset = 0.5, align=TRUE, barsize = 1)
-
-# Save graph
-ggsave(plot = tree_pl2,
-       filename = "Output/Phylo_tree_draft3.pdf", 
-       width = 10, height = 10, scale = 5, units = "cm")
-
-###
-node_labels <- sort(unique(SiteTree_gr$node.label))
-node_labels <- node_labels[-1]
-order_groups <- sort(unique(Phylo_info_merged$order_group))
-
-setdiff(order_groups, node_labels)
-
-
-###
-###############################################
-# Trial with geom_cladelabel
-###############################################
-
-mrca(SiteTree_gr)["Phyllospadix_scouleri", "Dichorisandra_incurva"]
-MRCA(tree_pl, tip=c('Phyllospadix_scouleri', 'Dichorisandra_incurva'))
-
-test_tree <- tree_pl + geom_cladelabel(node=1270, label="test label")
+    # add text group labels at the middle of each segment
+    geom_text(data = coord_groups,
+              aes(x = my_x,
+                  y = y_mid,
+                  angle = angle_adj,
+                  hjust = hjust_adj,
+                  label = order_group),
+              vjust = 0.5, 
+              size  = 5,
+              nudge_x = 5, # offsetting text
+              color = "black",
+              show.legend = FALSE) +
+    # adjust theme components
+    theme(legend.position = "right")
 
 ggsave(plot = test_tree,
-       filename = "Output/Phylo_tree_draft6.pdf", 
+       filename = "Output/Phylo_tree_draft8.pdf", 
        width = 10, height = 10, scale = 5, units = "cm")
 
-.node.desc(tree, 1243)
-
-
-
+# =============================================================================
+# References
+# =============================================================================
+# ggtree:
+#   https://bioconductor.org/packages/release/bioc/html/ggtree.html
+#   https://guangchuangyu.github.io/ggtree/
+#   https://github.com/GuangchuangYu/ggtree
+# tidytree:
+#   https://cran.r-project.org/web/packages/tidytree/vignettes/tidytree.html
+#   https://cran.r-project.org/web/packages/tidytree/index.html

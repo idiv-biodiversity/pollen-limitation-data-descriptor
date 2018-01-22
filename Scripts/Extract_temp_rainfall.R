2fb55d8e78824c8aa3fda46a1f38ec2bcf7ca4ab
valentinitnelav <stefan.valentin87@yahoo.com>
2018-01-22 16:42:32 +0100

    update object naming - tried to replace . with _ for all object names as . can create sometimes confusion with methods or package names, etc - update some comments - work with row PL dataset

--- Scripts/Extract_temp_rainfall.R
+++ Scripts/Extract_temp_rainfall.R
@@ -1,8 +1,8 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Script to extract Annual precipitation amount [mm] and Annual mean temperature [°C]  for each study location
-# in the pollen limitation dataset.
-# Extractions are used further for making the biomes Whittaker diagram. 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###############################################################################
+## Script to prepare data for plotting with Whittaker biomes.
+## Extracts Annual precipitation amount [mm] and Annual mean temperature [°C] 
+## for each study location in the row pollen limitation dataset.
+###############################################################################
 
 library(data.table)
 library(raster)
@@ -12,69 +12,82 @@ library(RANN)
 # Source buffer-extraction function from GitHub
 source("https://raw.githubusercontent.com/valentinitnelav/helpers/master/R/extract_FromNearCellRst.R")
 
-# set the temporary folder for raster package operations
-rasterOptions(tmpdir = "Data/tmp_rst")
+# =============================================================================
+# Read & prepare data
+# =============================================================================
+# Read latest dataset from Joanne
+PL_dt <- fread("Data/PL_masters_for publication.csv",
+               select = c("unique_number", 
+                          "lon_decemial_unpinned", 
+                          "lat_decimal_unpinned"),
+               colClasses = "character")
+# remove any empty rows
+PL_dt <- PL_dt[unique_number != ""]
 
-# read latest dataset from Joanne
-myDT <- fread("Data/PL_ANALYSIS_02_10_2017.csv", colClasses = "character")
+# Transform longitude to numeric
+PL_dt[, lon := as.numeric(lon_decemial_unpinned)]
+PL_dt[is.na(lon)] # the minus sign is not a ream minus sign
+PL_dt[unique_number == "2219", lon := -7.317197]
+PL_dt[, lon_decemial_unpinned := NULL]
 
-# get only columns of interest
-studyXY <- myDT[,.(unique_number, lon_decimal_PTL_JMB, lat_decimal_PTL_JMB)]
-studyXY[, lon_decimal_PTL_JMB := as.numeric(lon_decimal_PTL_JMB)]
-studyXY[, lat_decimal_PTL_JMB := as.numeric(lat_decimal_PTL_JMB)]
+# Transform latitude to numeric
+PL_dt[, lat := as.numeric(lat_decimal_unpinned)]
+PL_dt[, lat_decimal_unpinned := NULL]
 
-# some routine checking of coordinates values
-range(studyXY[,lon_decimal_PTL_JMB]) %between% c(-180, 180)
-range(studyXY[,lat_decimal_PTL_JMB]) %between% c(-90, 90)
+# Some routine checking of coordinates values
+range(PL_dt[,lon]) %between% c(-180, 180)
+range(PL_dt[,lat]) %between% c(-90, 90)
 
-# get unique pairs of coordinates
-studyXY.unq <- unique(studyXY[,.(lon_decimal_PTL_JMB, lat_decimal_PTL_JMB)], 
-                      by = c("lon_decimal_PTL_JMB", "lat_decimal_PTL_JMB") )
+# =============================================================================
+# Raster extractions
+# =============================================================================
+# Get unique pairs of coordinates
+xy_unq <- unique(PL_dt[,.(lon, lat)], by = c("lon", "lat") )
 
-# Read in rasters from iDiv share
-# Annual precipitation amount [mm] 
-rst.pp <- raster("I:/sie/_data_VS/CHELSA/chelsa_bioclim_v1.2/CHELSA_bio_12.tif")
-names(rst.pp) <- "Annual_pp_mm"
-# Annual mean temperature [°C] 
-rst.temp <- raster("I:/sie/_data_VS/CHELSA/chelsa_bioclim_v1.2/CHELSA_bio_1.tif")
-names(rst.temp) <- "Annual_mean_temp_C"
+# Read in rasters from iDiv share:
+# - Annual precipitation amount [mm] 
+rst_pp <- raster("I:/sie/_data_VS/CHELSA/chelsa_bioclim_v1.2/CHELSA_bio_12.tif")
+names(rst_pp) <- "Annual_pp_mm"
+# - Annual mean temperature [°C] 
+rst_temp <- raster("I:/sie/_data_VS/CHELSA/chelsa_bioclim_v1.2/CHELSA_bio_1.tif")
+names(rst_temp) <- "Annual_mean_temp_C"
 
-# set the buffer in meters
-buf.m <- 50000
+# Set the search buffer in meters. It will search for closest non-NA cell.
+buf_m <- 50000
 
 # Extract from list of rasters
 system.time(
-    extr.lst <- lapply(X   = list(rst.pp, rst.temp),
+    extr_lst <- lapply(X   = list(rst_pp, rst_temp),
                        FUN = extract_FromNearCellRst,
-                       XY  = studyXY.unq, 
-                       my.buffer  = buf.m,
+                       XY  = xy_unq, 
+                       my.buffer  = buf_m,
                        simplified = FALSE,
                        lib.check  = TRUE)
 )
 # takes approx 1 min
 
-# create a column for each extraction table that indicates which raster was used
-lapply(extr.lst, function(dt) dt[, varb := names(dt)[4]])
+# Create a column for each extraction table that indicates which raster was used
+lapply(extr_lst, function(dt) dt[, varb := names(dt)[4]])
 
 # rbind all table extractions from the extraction list
-extr.dt <- rbindlist(extr.lst)
+extr_dt <- rbindlist(extr_lst)
 
-# rename column 4
-data.table::setnames(extr.dt, 
+# Rename column 4
+data.table::setnames(extr_dt, 
                      old = 4, 
-                     new = "extracted.val")
+                     new = "extracted_val")
 
-# transform from long to wide format
-extr.dt.wide <- dcast(extr.dt, 
-                      lon_decimal_PTL_JMB + lat_decimal_PTL_JMB  ~ varb, 
-                      value.var = "extracted.val")
+# Transform from long to wide format
+extr_dt_wide <- dcast(extr_dt, 
+                      lon + lat ~ varb, 
+                      value.var = "extracted_val")
 
-# join unique extraction results with data for each location
-extr.res <- merge(x  = studyXY,
-                  y  = extr.dt.wide,
-                  by = c("lon_decimal_PTL_JMB",  "lat_decimal_PTL_JMB"),
+# Join unique extraction results with original table
+extr_res <- merge(x  = PL_dt,
+                  y  = extr_dt_wide,
+                  by = c("lon",  "lat"),
                   all.x = TRUE, 
                   sort  = FALSE)
-str(extr.res)
+str(extr_res)
 # save results
-write.csv(extr.res, "Output/extractions_temp_pp.csv", row.names = FALSE)
+write.csv(extr_res, "Output/extractions_temp_pp.csv", row.names = FALSE)
